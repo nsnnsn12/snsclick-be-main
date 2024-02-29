77906871a3521621e71fee3a8744eeb3cf59bf93
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-29 11:07:55 +0900

    add: Define property of ResponseItem

--- src/main/java/com/snsclicksystem/main/adapter/in/item/dto/ResponseItem.java
+++ src/main/java/com/snsclicksystem/main/adapter/in/item/dto/ResponseItem.java
@@ -1,4 +1,16 @@
 package com.snsclicksystem.main.adapter.in.item.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseItem {
+    private String itemType;
+    private Integer itemPrice;
+    private Integer minOrderQuantity;
+    private Integer maxOrderQuantity;
+    private Boolean isRepeatable;
+    private Boolean isCancelable;
+    private Integer averageCompletionTime;
 }
