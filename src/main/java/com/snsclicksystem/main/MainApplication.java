42ea7bf22bfefba4cdfd17325b156dae8237a53c
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-29 11:31:48 +0900

    feat: Disable spring security

--- src/main/java/com/snsclicksystem/main/MainApplication.java
+++ src/main/java/com/snsclicksystem/main/MainApplication.java
@@ -2,10 +2,12 @@ package com.snsclicksystem.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class MainApplication {
 
 	public static void main(String[] args) {
