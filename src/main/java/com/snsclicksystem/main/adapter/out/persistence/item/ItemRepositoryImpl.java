56a819a0476f82f619032094bd9d92c98df468ae
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-02 22:43:37 +0900

    feat: Declare ItemRepository.findByItemTypeAndGreaterItemPrice

--- src/main/java/com/snsclicksystem/main/adapter/out/persistence/item/ItemRepositoryImpl.java
+++ src/main/java/com/snsclicksystem/main/adapter/out/persistence/item/ItemRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.snsclicksystem.main.adapter.out.persistence.item;
 
+import com.snsclicksystem.main.application.port.out.persistence.item.ItemCriteria;
 import com.snsclicksystem.main.application.port.out.persistence.item.ItemRepository;
 import com.snsclicksystem.main.domain.item.SnsItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,6 +17,11 @@ public class ItemRepositoryImpl implements ItemRepository {
     public Optional<SnsItem> findById(Long id) {
         Optional<ItemEntity> item = jpaRepository.findById(id);
         return item.map(this::getSnsItemFromModel);
+    }
+
+    @Override
+    public List<SnsItem> findByItemTypeAndGreaterItemPrice(ItemCriteria criteria) {
+        return null;
     }
 
     private SnsItem getSnsItemFromModel(ItemEntity entity){
--- src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemRepository.java
+++ src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemRepository.java
@@ -2,8 +2,11 @@ package com.snsclicksystem.main.application.port.out.persistence.item;
 
 import com.snsclicksystem.main.domain.item.SnsItem;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository {
     Optional<SnsItem> findById(Long id);
+
+    List<SnsItem> findByItemTypeAndGreaterItemPrice(ItemCriteria criteria);
 }
