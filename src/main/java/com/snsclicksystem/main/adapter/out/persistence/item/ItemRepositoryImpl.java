f88ce19a299f327dc7d686ccaf0b1ea47acd7d31
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-02 22:54:24 +0900

    feat: Define ItemRepositoryImpl.findByItemTypeAndGreaterItemPrice

--- src/main/java/com/snsclicksystem/main/adapter/out/persistence/item/ItemRepositoryImpl.java
+++ src/main/java/com/snsclicksystem/main/adapter/out/persistence/item/ItemRepositoryImpl.java
@@ -1,18 +1,24 @@
 package com.snsclicksystem.main.adapter.out.persistence.item;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.snsclicksystem.main.application.port.out.persistence.item.ItemCriteria;
 import com.snsclicksystem.main.application.port.out.persistence.item.ItemRepository;
 import com.snsclicksystem.main.domain.item.SnsItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.snsclicksystem.main.adapter.out.persistence.item.QItemEntity.itemEntity;
 
 @Repository
 @RequiredArgsConstructor
 public class ItemRepositoryImpl implements ItemRepository {
     private final ItemJPARepository jpaRepository;
+    private final JPAQueryFactory jpaQueryFactory;
     @Override
     public Optional<SnsItem> findById(Long id) {
         Optional<ItemEntity> item = jpaRepository.findById(id);
@@ -21,7 +27,21 @@ public class ItemRepositoryImpl implements ItemRepository {
 
     @Override
     public List<SnsItem> findByItemTypeAndGreaterItemPrice(ItemCriteria criteria) {
-        return null;
+        List<ItemEntity> fetch = jpaQueryFactory.selectFrom(itemEntity)
+                .where(eqItemType(criteria.getItemType()),
+                        graterItemType(criteria.getItemPrice()))
+                .fetch();
+        return fetch.stream().map(this::getSnsItemFromModel).toList();
+    }
+
+    private BooleanExpression eqItemType(String itemType){
+        if(!StringUtils.hasText(itemType)) return null;
+        return itemEntity.itemType.eq(itemType);
+    }
+
+    private BooleanExpression graterItemType(Integer itemPrice){
+        if(itemPrice == null) return null;
+        return itemEntity.itemPrice.goe(itemPrice);
     }
 
     private SnsItem getSnsItemFromModel(ItemEntity entity){
