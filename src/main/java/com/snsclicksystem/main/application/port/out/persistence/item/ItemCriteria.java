5e2f008f817aa87d199a4a7ed8b666e53c4c32e6
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-02 22:53:59 +0900

    feat: Add getter and setter to ItemCriteria

--- src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemCriteria.java
+++ src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemCriteria.java
@@ -1,5 +1,10 @@
 package com.snsclicksystem.main.application.port.out.persistence.item;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ItemCriteria {
     private String itemType;
     private Integer itemPrice;
