9274388b8ad0a6737477fc6e4838b8b627ffccba
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-02 22:43:15 +0900

    feat: Define ItemCriteria

--- src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemCriteria.java
+++ src/main/java/com/snsclicksystem/main/application/port/out/persistence/item/ItemCriteria.java
@@ -1,4 +1,12 @@
 package com.snsclicksystem.main.application.port.out.persistence.item;
 
 public class ItemCriteria {
+    private String itemType;
+    private Integer itemPrice;
+    private Integer itemSolidPrice;
+    private Integer minOrderQuantity;
+    private Integer maxOrderQuantity;
+    private Boolean isRepeatable;
+    private Boolean isCancelable;
+    private Integer averageCompletionTime;
 }
