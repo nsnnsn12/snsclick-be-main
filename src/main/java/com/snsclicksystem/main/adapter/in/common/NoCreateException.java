713206f63461166121efe23629f22fd3c562554b
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-04 09:58:13 +0900

    refactor: Sort code line

--- src/main/java/com/snsclicksystem/main/adapter/in/common/NoCreateException.java
+++ src/main/java/com/snsclicksystem/main/adapter/in/common/NoCreateException.java
@@ -2,28 +2,22 @@ package com.snsclicksystem.main.adapter.in.common;
 
 public class NoCreateException extends RuntimeException {
 
-	 public NoCreateException() {
-	        super();
-	    }
+	public NoCreateException() {
+		super();
+	}
+	public NoCreateException(String message) {
+		super(message);
+	}
+	public NoCreateException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-	   public NoCreateException(String message) {
-	        super(message);
-	    }
+	public NoCreateException(Throwable cause) {
+		super(cause);
+	}
 
-	
-	    public NoCreateException(String message, Throwable cause) {
-	        super(message, cause);
-	    }
-
-	 
-	    public NoCreateException(Throwable cause) {
-	        super(cause);
-	    }
-
-	    protected NoCreateException(String message, Throwable cause,
-	                               boolean enableSuppression,
-	                               boolean writableStackTrace) {
-	        super(message, cause, enableSuppression, writableStackTrace);
-	    }
+	protected NoCreateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 
 }
