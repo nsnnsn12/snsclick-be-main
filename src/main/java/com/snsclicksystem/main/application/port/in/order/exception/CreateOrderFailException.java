fbe41d2e896b6b43519045db52fa4dc7354c3601
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-04 10:01:38 +0900

    refactor: Sort code line

--- src/main/java/com/snsclicksystem/main/application/port/in/order/exception/CreateOrderFailException.java
+++ src/main/java/com/snsclicksystem/main/application/port/in/order/exception/CreateOrderFailException.java
@@ -1,32 +1,32 @@
 package com.snsclicksystem.main.application.port.in.order.exception;
 
-public class CreateOrderFailException extends Exception{
-	
-	private static final String message = "주문 실패";
-	
-	
-	   public CreateOrderFailException() {
-	        super(message);
-	    }
+public class CreateOrderFailException extends Exception {
 
-	   public CreateOrderFailException(String message) {
-	        super(message);
-	    }
+    private static final String message = "주문 실패";
 
-	
-	    public CreateOrderFailException(String message, Throwable cause) {
-	        super(message, cause);
-	    }
 
-	 
-	    public CreateOrderFailException(Throwable cause) {
-	        super(cause);
-	    }
+    public CreateOrderFailException() {
+        super(message);
+    }
 
-	    protected CreateOrderFailException(String message, Throwable cause,
-	                               boolean enableSuppression,
-	                               boolean writableStackTrace) {
-	        super(message, cause, enableSuppression, writableStackTrace);
-	    }
+    public CreateOrderFailException(String message) {
+        super(message);
+    }
+
+
+    public CreateOrderFailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public CreateOrderFailException(Throwable cause) {
+        super(cause);
+    }
+
+    protected CreateOrderFailException(String message, Throwable cause,
+                                       boolean enableSuppression,
+                                       boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 
 }
--- src/main/java/com/snsclicksystem/main/application/port/in/order/exception/UserAmountNotEnoughException.java
+++ src/main/java/com/snsclicksystem/main/application/port/in/order/exception/UserAmountNotEnoughException.java
@@ -1,29 +1,29 @@
 package com.snsclicksystem.main.application.port.in.order.exception;
 
-public class UserAmountNotEnoughException extends CreateOrderFailException{
-	private static final String message = "사용자 금액 부족";
-	
-	   public UserAmountNotEnoughException() {
-	        super(message);
-	    }
+public class UserAmountNotEnoughException extends CreateOrderFailException {
+    private static final String message = "사용자 금액 부족";
 
-	   public UserAmountNotEnoughException(String message) {
-	        super(message);
-	    }
+    public UserAmountNotEnoughException() {
+        super(message);
+    }
 
-	
-	    public UserAmountNotEnoughException(String message, Throwable cause) {
-	        super(message, cause);
-	    }
+    public UserAmountNotEnoughException(String message) {
+        super(message);
+    }
 
-	 
-	    public UserAmountNotEnoughException(Throwable cause) {
-	        super(cause);
-	    }
 
-	    protected UserAmountNotEnoughException(String message, Throwable cause,
-	                               boolean enableSuppression,
-	                               boolean writableStackTrace) {
-	        super(message, cause, enableSuppression, writableStackTrace);
-	    }
+    public UserAmountNotEnoughException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public UserAmountNotEnoughException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UserAmountNotEnoughException(String message, Throwable cause,
+                                           boolean enableSuppression,
+                                           boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
