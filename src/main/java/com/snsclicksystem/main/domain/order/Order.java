53d71bae143af5d2fd182c06779d5dc91d86b851
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-07 11:15:00 +0900

    implement: Add properties to Order

--- src/main/java/com/snsclicksystem/main/domain/order/Order.java
+++ src/main/java/com/snsclicksystem/main/domain/order/Order.java
@@ -1,5 +1,12 @@
 package com.snsclicksystem.main.domain.order;
 
-public class Order {
+import com.snsclicksystem.main.domain.item.SnsItem;
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+@Getter
+public class Order {
+    private SnsItem item;
+    private Long consumerId;
 }
