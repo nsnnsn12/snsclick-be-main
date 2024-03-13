db3f4c8d13e177a33a806333d67063d0d50dc350
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-13 17:49:54 +0900

    add: Add dtos related to real site

--- src/main/java/com/snsclicksystem/main/adapter/out/api/order/realsite/dto/BaseOrderDto.java
+++ src/main/java/com/snsclicksystem/main/adapter/out/api/order/realsite/dto/BaseOrderDto.java
@@ -1,4 +1,4 @@
-package com.snsclicksystem.main.adapter.out.api.order.dto;
+package com.snsclicksystem.main.adapter.out.api.order.realsite.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/snsclicksystem/main/adapter/out/api/order/realsite/dto/MentionsCustomListOrderDto.java
+++ src/main/java/com/snsclicksystem/main/adapter/out/api/order/realsite/dto/MentionsCustomListOrderDto.java
@@ -1,4 +1,4 @@
-package com.snsclicksystem.main.adapter.out.api.order.dto;
+package com.snsclicksystem.main.adapter.out.api.order.realsite.dto;
 
 import lombok.Getter;
 import lombok.Setter;
