ffa64dfd204c27a374b2264775ce793f2936b960
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-14 15:23:04 +0900

    refactor: modify Charge class to Charge abstract class

--- src/main/java/com/snsclicksystem/main/domain/charge/Charge.java
+++ src/main/java/com/snsclicksystem/main/domain/charge/Charge.java
@@ -1,67 +1,20 @@
 package com.snsclicksystem.main.domain.charge;
 
-import java.time.LocalDateTime;
 
-import com.snsclicksystem.main.adapter.in.charge.dto.RequestCharge;
-import com.snsclicksystem.main.config.TossPaymentConfig;
-import com.snsclicksystem.main.domain.member.Member;
+import com.snsclicksystem.main.application.service.charge.ChargeParameter;
+import com.snsclicksystem.main.domain.charge.dto.ChargedInfo;
+import com.snsclicksystem.main.domain.charge.exception.ChargeRequestFailException;
+import lombok.RequiredArgsConstructor;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+@RequiredArgsConstructor
+public abstract class Charge {
+	protected final ChargeParameter parameter;
 
-@Getter
-@Builder
-@ToString
-public class Charge {
-	
-	private Long id;
-	
-	private String chargeType;
-	
-	private Long realAmountPaid;
-	
-	private String cancelReason;
-	
-	private Boolean isCancel;
-	
-	private String failReason;
-	
-	private Boolean isPaySuccess;
-	
-	private String chargeApiId;
-	
-	private LocalDateTime createdAt;
-	
-	private String successUrl;
-	
-	private String failUrl;
-	
-	private Long memberId;
-	
-	private String memberEmail;
-	
-	public void setSuccessUrl(String successUrl) {
-		this.successUrl = successUrl;
-	}
-	
-	public void setFailUrl(String failUrl) {
-		this.failUrl = failUrl;
-	}
-	
-	public void setMemberEmail(String email) {
-		this.memberEmail = email;
-	}
-	
-	public static Charge createRequestCharge(Long memberId, String chargeType, Long realAmountPaid, String successUrl, String failUrl) {
-		return Charge.builder().memberId(memberId)
-		.chargeType(chargeType)
-		.realAmountPaid(realAmountPaid)
-		.successUrl(successUrl)
-		.failUrl(failUrl).build();
+	public ChargedInfo execute() throws ChargeRequestFailException {
+		//TODO 충전이라는 행위 전후에 처리해야 하는 비즈니스 로직이 있는 경우 작성 필요
+		return charge();
 	}
 
+	//TODO 이 부분에 각각의 자식 클래스가 필요한 충전 방식을 구현
+	protected abstract ChargedInfo charge() throws ChargeRequestFailException;
 }
