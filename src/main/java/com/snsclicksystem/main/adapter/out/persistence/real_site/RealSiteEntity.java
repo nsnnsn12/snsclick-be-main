67961a390cb01fef8bd9729c9d3c5c751c7cea99
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-20 11:22:21 +0900

    refactor: Change type from Long to Item in RealSiteEntity

--- src/main/java/com/snsclicksystem/main/adapter/out/persistence/real_site/RealSiteEntity.java
+++ src/main/java/com/snsclicksystem/main/adapter/out/persistence/real_site/RealSiteEntity.java
@@ -1,18 +1,18 @@
 package com.snsclicksystem.main.adapter.out.persistence.real_site;
 
 import com.snsclicksystem.main.adapter.out.persistence.common.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import com.snsclicksystem.main.adapter.out.persistence.item.ItemEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity(name="real_site")
 @Getter
 public class RealSiteEntity extends BaseTimeEntity {
     private Long id;
-    @Column(unique = true)
-    private Long itemId;
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private ItemEntity item;
+
     @Column(unique = true)
     private Long serviceId;
     private String name;
