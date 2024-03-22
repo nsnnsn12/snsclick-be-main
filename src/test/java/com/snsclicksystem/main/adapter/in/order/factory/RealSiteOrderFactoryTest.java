6fde8097cc753da13656f8333d55f1d957106030
NohSungGyu <gkdlshtjdrb@naver.com>
2024-03-22 13:12:28 +0900

    test: Add test class for RealSiteOrderFactory

---
+++ src/test/java/com/snsclicksystem/main/adapter/in/order/factory/RealSiteOrderFactoryTest.java
@@ -0,0 +1,8 @@
+package com.snsclicksystem.main.adapter.in.order.factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RealSiteOrderFactoryTest {
+    //정상적으로 order를 생성하는 경우
+    //
+}
